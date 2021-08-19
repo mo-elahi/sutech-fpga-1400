48c03822145817dff16cb531b3887f599230043d
mo-elahi <moh.elahi.80@gmail.com>
2021-08-19 22:10:51 +0430

    clkdiv and bin2hex behaviors are added
    DONE
    ====================
    - clkdiv and bin2hex behaviors are added
    
    TODO
    =====================
    - add behavior logic to digital_clock
    - complete test bench file code

--- digital_clock/digital_clock.srcs/sources_1/new/bin2hex.vhd
+++ digital_clock/digital_clock.srcs/sources_1/new/bin2hex.vhd
@@ -8,5 +8,25 @@ port (
 end bin2hex;
 architecture Behavioral of bin2hex is
 begin
- 
+ process(Bin)
+ begin
+  case(Bin) is
+   when "0000" =>  Hout <= "1000000"; --0--
+   when "0001" =>  Hout <= "1111001"; --1--
+   when "0010" =>  Hout <= "0100100"; --2--
+   when "0011" =>  Hout <= "0110000"; --3--
+   when "0100" =>  Hout <= "0011001"; --4-- 
+   when "0101" =>  Hout <= "0010010"; --5--    
+   when "0110" =>  Hout <= "0000010"; --6--
+   when "0111" =>  Hout <= "1111000"; --7--   
+   when "1000" =>  Hout <= "0000000"; --8--
+   when "1001" =>  Hout <= "0010000"; --9--
+   when "1010" =>  Hout <= "0001000"; --a--
+   when "1011" =>  Hout <= "0000011"; --b--
+   when "1100" =>  Hout <= "1000110"; --c--
+   when "1101" =>  Hout <= "0100001"; --d--
+   when "1110" =>  Hout <= "0000110"; --e--
+   when others =>  Hout <= "0001110"; 
+   end case;
+ end process;
 end Behavioral;
--- digital_clock/digital_clock.srcs/sources_1/new/clkdiv.vhd
+++ digital_clock/digital_clock.srcs/sources_1/new/clkdiv.vhd
@@ -9,6 +9,19 @@ entity clk_div is
 end clk_div;
 
 architecture Behavioral of clk_div is
-begin
 
+signal counter: std_logic_vector(27 downto 0):=(others =>'0');
+begin
+ process(clk_50)
+	begin
+			if(rising_edge(clk_50)) then
+					counter <= counter + x"0000001";
+					if(counter>=x"F4240") then --50000000 shomaresh shodeh 1 saneyeh
+						counter <= x"0000000";
+					end if;
+			end if;
+ end process;
+ 
+ clk_1s <= '0' when counter < x"7A120" else '1';--25000000 shomaresh shodeh nesf d; saneyeh
+ 
 end Behavioral;
